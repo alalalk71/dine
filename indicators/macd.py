5f483982c27d118effb6cf358c5ae89b80c05f2a
alalalk71 <129861984+alalalk71@users.noreply.github.com>
2025-09-28 19:20:06 +0330

    Delete indicators directory

--- indicators/macd.py
+++
@@ -1,7 +0,0 @@
-def compute_macd(series, fast=12, slow=26, signal=9):
-    ema_fast = series.ewm(span=fast, adjust=False).mean()
-    ema_slow = series.ewm(span=slow, adjust=False).mean()
-    macd_line = ema_fast - ema_slow
-    signal_line = macd_line.ewm(span=signal, adjust=False).mean()
-    hist = macd_line - signal_line
-    return macd_line, signal_line, hist
